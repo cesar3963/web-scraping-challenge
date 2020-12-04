8d9bd41a4dafee56a4aa84254950c261de6d46a2
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-12-03 21:47:09 -0500

    working on it

--- mars_flask_app.py
+++ mars_flask_app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+import Pyclient
 import mars_scrape
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+conn = "clientdb://localhost:27017/mars_app"
+client = Pyclient.clientClient(conn)
+
+# app.config["client_URI"] = "clientdb://localhost:27017/mars_app"
+# client = Pyclient(app)
 
 @app.route("/")
 def index():
 
-    mars_dict = mongo.db.mars_dict.find_one()
+    mars_dict = client.db.mars_dict.find_one()
     return render_template("index.html", mars=mars_dict)
 
 
 @app.route("/mars_scrape.py")
 def scrape():
   
-    mars_dict = mongo.db.mars_dict
+    mars_dict = client.db.mars_dict
     mars_data = mars_scrape.scrape()
     mars_dict.update({}, mars_data, upsert=True)
     return redirect("/")
