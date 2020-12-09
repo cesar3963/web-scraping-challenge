f969746c325771561e5a02612070d06b6f657b91
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-12-08 21:21:20 -0500

    final fixes

--- Missions_to_Mars/mars_scrape.py
+++ Missions_to_Mars/mars_scrape.py
@@ -4,7 +4,7 @@ import pandas as pd
 import datetime as dt 
 
 def scrape_all():
-    browser = Browser("chrome", executable_path="C:/Users/cesar/Documents/GitHub/web-scraping-challenge/Missions_to_Mars/static/chromedriver.exe", headless=True)
+    browser = Browser("chrome", executable_path="chromedriver.exe", headless=True)
     news_title, news_paragraph = mars_news(browser)      
 
     data = {
@@ -22,7 +22,7 @@ def mars_news(browser):
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
 
-    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    browser.is_element_present_by_css("ul.item_list li.slide")
     html = browser.html
     news_soup = soup(html, 'html.parser')
 
@@ -39,7 +39,7 @@ def featured_image(browser):
     elem_img.click()
 
     
-    browser.is_element_present_by_text('more info',wait_time=...)
+    browser.is_element_present_by_text('more info')
     more_info = browser.links.find_by_partial_text('more info')
     more_info.click()
 
