f6b257c89c0a3d326cb656721eb2b49c13254512
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-12-09 17:52:11 -0500

    still trying

--- Missions_to_Mars/mars_scrape.py
+++ Missions_to_Mars/mars_scrape.py
@@ -5,12 +5,14 @@ import datetime as dt
 
 def scrape_all():
     browser = Browser("chrome", executable_path="chromedriver.exe", headless=True)
+    
     news_title, news_paragraph = mars_news(browser)      
 
     data = {
         "news_title": news_title, 
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
+        "facts": mars_facts(),
         "hemispheres": hemispheres(browser),
         "last_modified": dt.datetime.now() 
     }
@@ -23,14 +25,20 @@ def mars_news(browser):
     browser.visit(url)
 
     browser.is_element_present_by_css("ul.item_list li.slide")
+    
     html = browser.html
     news_soup = soup(html, 'html.parser')
 
-    elem_list = news_soup.select_one("ul.item_list li.slide")
-    tittle = elem_list.find("div", class_="content_title").get_text()
-    paragraph = elem_list.find("div", class_="article_teaser_body").get_text()
+    try:
+        elem_list = news_soup.select_one("ul.item_list li.slide")
+        tittle = elem_list.find("div", class_="content_title").get_text()
+        paragraph = elem_list.find("div", class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
 
     return tittle,paragraph
+
+
 def featured_image(browser):
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     html = browser.visit(url)
@@ -39,21 +47,28 @@ def featured_image(browser):
     elem_img.click()
 
     
-    browser.is_element_present_by_text('more info')
+    browser.is_element_present_by_text('more info', wait_time=1 )
     more_info = browser.links.find_by_partial_text('more info')
     more_info.click()
 
     html = browser.html
     img_soup = soup(html, 'html.parser')     
 
-    img_url = img_soup.select_one("figure.lede a img").get("src")
+    try:
+        img_url = img_soup.select_one("figure.lede a img").get("src")
+    except AttributeError:
+        return None
+    img_url_ = f'https://www.jpl.nasa.gov{img_url}'
+
     return img_url
 
 
 
 def mars_facts():
-    df = pd.read_html('http://space-facts.com/mars/')[0]
-
+    try:
+        df = pd.read_html('http://space-facts.com/mars/')[0]
+    except BaseException:
+        return None
     df.columns=['Description', 'Mars']
     df.set_index('Description', inplace=True)
 
@@ -63,19 +78,24 @@ def mars_facts():
 def hemispheres(browser):
     url= ('https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars')
     browser.visit(url)
+    
     hemisphere_image_urls = []
-
     for i in range(4):
         browser.find_by_css("a.product-item h3")[i].click()
         hemi_data = scrape_hemisphere(browser.html)
         hemisphere_image_urls.append(hemi_data)
         browser.back()
+    return hemisphere_image_urls
    
 def scrape_hemisphere(html_text):
     hemi_soup =soup(html_text, 'html.parser')
 
-    title_elem = hemi_soup.find("h2", class_="title").get_text()
-    sample_elem = hemi_soup.find("a", text="Sample").get("href")
+    try:
+        title_elem = hemi_soup.find("h2", class_="title").get_text()
+        sample_elem = hemi_soup.find("a", text="Sample").get("href")
+    except AttributeError:
+        title_elem = None
+        sample_elem = None
 
     hemispheres = {
         "title": title_elem,
